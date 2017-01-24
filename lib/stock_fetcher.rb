bbf7bf6e308a64bbf7c80e0b0b39d2819e001ee6
Uriel Silva <urielbcs@gmail.com>
2017-01-24 09:27:02 -0200

    tests for fetching stocks

--- lib/stock_fetcher.rb
+++ lib/stock_fetcher.rb
@@ -1,3 +1,21 @@
+require 'net/http'
+
 class StockFetcher
+  # endpoint URL
+  API_URL = "https://www.google.com/finance/info?q="
+
+  def self.fetch(code)
+    url = URI.parse("#{API_URL}#{code}")
+    response =  Net::HTTP::get_response(url)
+    #removing // from the response
+    parse_response(response)
+  end
+
+  private
   
+  def self.parse_response(response)
+    JSON.parse response.body[3..response.body.length] unless response.code == "400"
+  end
+
+
 end
--- spec/fetcher_spec.rb
+++ spec/fetcher_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 require 'rails_helper'
 require 'stock_fetcher'
 
-Rspec.describe 'Stock Fetcher' do
-  it 'fetches all stocks' do
-
+RSpec.describe 'Stock Fetcher' do
+  it 'fetches a stock from a given code' do
+    code = 'PETR4'
+    expect(StockFetcher.fetch(code)).to_not be_empty
   end
+
+  it 'returns nil when the code is not found' do
+    code = 'LOTR'
+    expect(StockFetcher.fetch(code)).to be_nil
+  end
+
 end
