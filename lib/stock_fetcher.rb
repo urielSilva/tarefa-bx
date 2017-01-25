547e05a15e82f864485d5234185967dde6f8c507
Uriel Silva <urielbcs@gmail.com>
2017-01-25 02:12:18 -0200

    using http gem for request

--- lib/stock_fetcher.rb
+++ lib/stock_fetcher.rb
@@ -1,28 +1,17 @@
-require 'net/http'
-require 'uri'
+
 class StockFetcher
   # endpoint URL
-  API_URL = "http://www.google.com/finance/info?q="
+  API_URL = "https://www.google.com/finance/info?q="
 
   def self.fetch(code)
-    # res = HTTP.get("#{API_URL}#{code}")
-
-    uri = URI.parse("#{API_URL}#{code}")
-    res = Net::HTTP.new(uri.host).get2(uri, { 'User-Agent' => 'anything' } )
-    # req = Net::HTTP::Get.new(uri.to_s)
-    # req.add_field("User-Agent", "anything")
-    # res = Net::HTTP.new(uri.host, uri.port).start do |http|
-    #   http.request(req)
-    # end
-
+    res = HTTP.get("#{API_URL}#{code}")
     parse_response(res)
-
   end
 
   private
 
   def self.parse_response(response)
-    JSON.parse(response.body[3..response.body.length]).first unless response.code == "400"
+    JSON.parse(response.to_s[3..response.to_s.length]).first unless response.code == 400
   end
 
 
