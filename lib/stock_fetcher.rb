3f4f8630d955b5e328d9cc9e16781b49540f7aea
Uriel Silva <urielbcs@gmail.com>
2017-01-25 01:30:29 -0200

    fixing http request for heroku

--- lib/stock_fetcher.rb
+++ lib/stock_fetcher.rb
@@ -1,17 +1,18 @@
 require 'net/http'
-
+require 'uri'
 class StockFetcher
   # endpoint URL
-  API_URL = "https://www.google.com/finance/info?q="
+  API_URL = "http://www.google.com/finance/info?q="
 
   def self.fetch(code)
 
-    url = URI.parse("#{API_URL}#{code}")
-    response =  Net::HTTP::get_response(url)
-    #removing // from the response
-    puts url
-    puts response
-    parse_response(response)
+    uri = URI.parse("#{API_URL}#{code}")
+    req = Net::HTTP::Get.new(uri.to_s)
+    req.add_field("X-Forwarded-For", "0.0.0.0")
+    res = Net::HTTP.new(uri.host, uri.port).start do |http|
+      http.request(req)
+    end
+    parse_response(res)
 
   end
 
