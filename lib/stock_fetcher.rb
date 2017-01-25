e5e8c90a1ff133c4f0983d6add5bb09edf207cb3
Uriel Silva <urielbcs@gmail.com>
2017-01-25 02:09:30 -0200

    changing http request

--- lib/stock_fetcher.rb
+++ lib/stock_fetcher.rb
@@ -1,21 +1,20 @@
+require 'net/http'
+require 'uri'
 class StockFetcher
   # endpoint URL
-  API_URL = "https://www.google.com/finance/info?q="
+  API_URL = "http://www.google.com/finance/info?q="
 
   def self.fetch(code)
-    res = HTTP.get("#{API_URL}#{code}")
+    # res = HTTP.get("#{API_URL}#{code}")
 
-    # uri = URI.parse("#{API_URL}#{code}")
-    # Net::HTTP.new(uri.to_s).get2(uri, { 'User-Agent' => 'anything' } )
+    uri = URI.parse("#{API_URL}#{code}")
+    res = Net::HTTP.new(uri.host).get2(uri, { 'User-Agent' => 'anything' } )
     # req = Net::HTTP::Get.new(uri.to_s)
     # req.add_field("User-Agent", "anything")
     # res = Net::HTTP.new(uri.host, uri.port).start do |http|
     #   http.request(req)
     # end
-    puts "#{API_URL}#{code}"
-    puts res.to_s
-    puts res
-    puts res.code
+
     parse_response(res)
 
   end
@@ -23,7 +22,7 @@ class StockFetcher
   private
 
   def self.parse_response(response)
-    JSON.parse(response.to_s[3..response.to_s.length]).first unless response.code == 400
+    JSON.parse(response.body[3..response.body.length]).first unless response.code == "400"
   end
 
 
