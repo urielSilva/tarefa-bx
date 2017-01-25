121d288510f8dbf0c1a88a372ad9b393353ec6bb
Uriel Silva <urielbcs@gmail.com>
2017-01-25 11:07:25 -0200

    adding ssl for request

--- lib/stock_fetcher.rb
+++ lib/stock_fetcher.rb
@@ -1,17 +1,34 @@
-
+require 'net/http'
+require 'uri'
 class StockFetcher
   # endpoint URL
   API_URL = "https://www.google.com/finance/info?q="
 
   def self.fetch(code)
-    res = HTTP.get("#{API_URL}#{code}")
+    # res = HTTP.get("#{API_URL}#{code}")
+
+    uri = URI.parse("#{API_URL}#{code}")
+
+    req = Net::HTTP::Get.new("#{API_URL}#{code}")
+    req.add_field("User-Agent", "anything")
+    http =  Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    res = http.start do |http|
+      http.request(req)
+    end
+    puts res.body
+    puts "#{API_URL}#{code}"
+    puts res.code
+    puts req
     parse_response(res)
+
   end
 
   private
 
   def self.parse_response(response)
-    JSON.parse(response.to_s[3..response.to_s.length]).first unless response.code == 400
+    JSON.parse(response.body[3..response.body.length]).first unless response.code == "400"
   end
 
 
